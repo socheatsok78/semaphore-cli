e646df599fc69a3b578b280b99bef599c1211609
Socheat Sok <socheatsok78@gmail.com>
2024-10-11 17:59:06 +0700

    Refactor username and password flags

--- cmd/semaphore.go
+++ cmd/semaphore.go
@@ -12,30 +12,30 @@ var (
 	// Version is the version of the CLI
 	Version = "dev"
 
-	// Flag variables
-	usernameStdinFlag bool
-	passwordStdinFlag bool
-
 	// Command-line root command
 	rootCmd = &cobra.Command{
 		Use:     "semaphore",
 		Short:   "A backup and restore tool for Semaphore CI",
 		Version: Version,
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if usernameStdinFlag {
+			username := cmd.Flag("username").Value.String()
+			if username == "" {
 				username, err := readUsername()
 				if err != nil {
 					return err
 				}
 				cmd.Flags().Set("username", username)
 			}
-			if passwordStdinFlag {
+
+			password := cmd.Flag("password").Value.String()
+			if password == "" {
 				password, err := readPassword()
 				if err != nil {
 					return err
 				}
 				cmd.Flags().Set("password", password)
 			}
+
 			return nil
 		},
 	}
@@ -44,18 +44,8 @@ var (
 func init() {
 	rootCmd.PersistentFlags().String("dns", "127.0.0.11", "dns resolver")
 	rootCmd.PersistentFlags().String("addr", "https://cloud.semaphoreui.com", "the semaphore address")
-
-	// Username flags
 	rootCmd.PersistentFlags().String("username", "", "the semaphore username")
-	rootCmd.MarkPersistentFlagRequired("username")
-	rootCmd.PersistentFlags().BoolVar(&usernameStdinFlag, "username-stdin", false, "read username from stdin")
-	rootCmd.MarkFlagsOneRequired("username", "username-stdin")
-
-	// Password flags
 	rootCmd.PersistentFlags().String("password", "", "the semaphore password")
-	rootCmd.MarkPersistentFlagRequired("password")
-	rootCmd.PersistentFlags().BoolVar(&passwordStdinFlag, "password-stdin", false, "read password from stdin")
-	rootCmd.MarkFlagsOneRequired("password", "password-stdin")
 }
 
 func readUsername() (string, error) {
@@ -63,7 +53,7 @@ func readUsername() (string, error) {
 	var username string
 	_, err := fmt.Scanln(&username)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("username cannot be empty")
 	}
 	if username == "" {
 		return "", fmt.Errorf("username cannot be empty")
